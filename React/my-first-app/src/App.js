fb5f66063080ce3aeba6b803c11d3213e3f7303f
Manoj-walker <challamanoj9949@gmail.com>
2026-06-01 16:23:37 +0530

    my first react app

---
+++ React/my-first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App(){
+
+
+  return(
+    <div>
+      <h1 style={{color:'red',textAlign:'center'}}>My first - React App</h1>
+      <p>Hello Guys this is my First React app.</p>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SGvshARHJ5GYSH_Kig8-cYNw5rO3nWn7mA&s"></img>
+    </div>
+  )
+}
+
+export default App
